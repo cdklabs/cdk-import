0fffaef460ed957af686b0f24324fdf0827a6adc
Elad Ben-Israel <benisrae@amazon.com>
2021-07-12 18:26:20 +0300

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,13 @@
+const { TypeScriptProject } = require('projen');
+const project = new TypeScriptProject({
+  defaultReleaseBranch: 'main',
+  name: 'cdk-cfn2ts',
+
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // release: undefined,                /* Add release management to this project. */
+});
+project.synth();
