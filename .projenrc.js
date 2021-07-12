65b91370f4c403420ca4c38d5107f47e87f2f89e
Elad Ben-Israel <benisrae@amazon.com>
2021-07-12 18:32:51 +0300

    rename project and add playground

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { TypeScriptProject } = require('projen');
 const project = new TypeScriptProject({
   defaultReleaseBranch: 'main',
-  name: 'cdk-cfn2ts',
+  name: 'cdk-import',
 
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
---
+++ playground/index.js
@@ -0,0 +1,11 @@
+const json2jsii = require('json2jsii');
+const schema = require('./eks.schema.json');
+const fs = require('fs');
+
+const gen = new json2jsii.TypeGenerator({
+  definitions: schema.definitions
+});
+
+gen.emitType('ClusterProps', schema);
+
+fs.writeFileSync('out.ts', gen.render());
