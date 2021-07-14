2a6aff603de482839bcd3a021b97004376801cfe
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2021-07-15 00:32:24 +0200

    first steps to implement import

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,20 @@
 const { TypeScriptProject } = require('projen');
 const project = new TypeScriptProject({
-  defaultReleaseBranch: 'main',
   name: 'cdk-import',
+  description: '',
+  defaultReleaseBranch: 'main',
+  deps: [
+    'json2jsii',
+    'aws-sdk',
+    'minimist',
+    'proxy-agent',
+  ],
+  bin: {
+    'cfn-import': 'lib/index.js',
+  },
+  devDeps: [
+    'ts-node',
+  ],
 
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // release: undefined,                /* Add release management to this project. */
 });
 project.synth();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,55 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
-  }
+// import { appendFileSync, readFileSync } from 'fs';
+import { Agent } from 'http';
+// import * as os from 'os';
+// import * as path from 'path';
+import * as AWS from 'aws-sdk';
+import * as minimist from 'minimist';
+import * as agent from 'proxy-agent';
+
+const args = minimist(process.argv.slice(2), {
+  string: [
+    'outdir',
+  ],
+  alias: {
+    outdir: 'o',
+  },
+});
+
+const awsOptions = {
+  ...(process.env.HTTPS_PROXY || process.env.https_proxy) && {
+    httpOptions: {
+      agent: agent(process.env.HTTPS_PROXY || process.env.https_proxy) as any as Agent,
+    },
+  },
+};
+
+async function getResourceDefinitonFromRegistry(name: string, version?: string): Promise<AWS.CloudFormation.DescribeTypeOutput> {
+  const cfn = new AWS.CloudFormation(awsOptions);
+  const type = await cfn.describeType({
+    Type: name.endsWith('MODULE') ? 'MODULE' : 'RESOURCE',
+    TypeName: name,
+    VersionId: version,
+  }).promise();
+  return type;
 }
+
+void (async () => {
+  if (args._.length !== 1) {
+    console.log('Please specify a resource name');
+    process.exit(1);
+  }
+  try {
+    const [resourceName, resourceVersion] = args._[0].split('@');
+    const outdir = args.outdir ?? 'src';
+    console.log(resourceName);
+    console.log(resourceVersion);
+    console.log(outdir);
+
+    const schema = await getResourceDefinitonFromRegistry(resourceName, resourceVersion);
+    console.log(schema);
+
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
