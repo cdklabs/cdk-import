98ef1932ea0670432648b16c924180057761de0c
Elad Ben-Israel <benisrae@amazon.com>
2021-11-09 12:51:33 +0200

    fix: module classes are named `CfnModule` instead `CfnXxxModule`
    Special-case modules so that the name of the class includes the type name as well and not always named `CfnModule`.
    
    Fixes #61

--- test/util.test.ts
+++ test/util.test.ts
@@ -3,6 +3,10 @@ import * as testee from '../src/util';
 test('should sanitize type names correctly', () => {
   expect(testee.sanitizeTypeName('Vendor::Service')).toBe('Service');
   expect(testee.sanitizeTypeName('Vendor::Service::Resource')).toBe('Resource');
-  expect(testee.sanitizeTypeName('Vendor::ServiceName')).toBe('Servicename');
+  expect(testee.sanitizeTypeName('Vendor::ServiceName')).toBe('ServiceName');
   expect(testee.sanitizeTypeName('Vendor::ServiceName::Resource')).toBe('Resource');
+});
+
+test('modules should include the type name with a Module suffix', () => {
+  expect(testee.sanitizeTypeName('JFrog::Linux::Bastion::MODULE')).toBe('BastionModule');
 });
