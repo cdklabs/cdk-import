ce4e6d71a9e10e7457043ad232284ce88f3873ec
Dillon Ponzo <dponzo18@gmail.com>
2022-04-15 10:15:26 -0400

    Add full describe call path and modify APIs
    This commit does not include any reachable code paths.
    We are opting to reduce number of needed API calls which we originally had before
    with multiple calls to fetch provisioning artifacts and launch paths separately in order to
    handle conerns over pagination. Instead we will just call describe product which returns
    everything in the response body with minimal latency differences.
    
    This also sets up adding both ability to describe a single product, as well as describing
    all available products, which we have default paths for selecting the PA and launch path for.
    
    Co-authored-by: Aidan Crank <arcrank@gmail.com>

--- test/service-catalog.test.ts
+++ test/service-catalog.test.ts
@@ -7,10 +7,7 @@ beforeEach(() => {
     searchProducts: jest.fn().mockImplementation(async () => {
       throw new Error('Not implemented');
     }),
-    listLaunchPaths: jest.fn().mockImplementation(async () => {
-      throw new Error('Not implemented');
-    }),
-    listProvisioningArtifacts: jest.fn().mockImplementation(async () => {
+    describeProduct: jest.fn().mockImplementation(async () => {
       throw new Error('Not implemented');
     }),
     describeProvisioningParameters: jest.fn().mockImplementation(async () => {
@@ -19,7 +16,7 @@ beforeEach(() => {
   };
 }),
 
-test('search products should fetch query product view summary', async () => {
+test('search products should fetch product view summaries', async () => {
   const productId = 'prod-abc123';
 
   client.searchProducts = jest.fn().mockImplementation( () => {
@@ -30,7 +27,7 @@ test('search products should fetch query product view summary', async () => {
     };
   });
 
-  const availableProducts = await testee.fetchAvailableProducts({ client: client, productId: productId });
+  const availableProducts = await testee.fetchAvailableProducts(client);
   expect(availableProducts[0].ProductId).toBe(productId);
   expect(availableProducts.length).toBe(1);
 });
@@ -58,115 +55,55 @@ test('search products should handle pagination', async () => {
     }
   });
 
-  const availableProducts = await testee.fetchAvailableProducts( { client: client });
+  const availableProducts = await testee.fetchAvailableProducts(client);
   expect(availableProducts[0].ProductId).toBe(productId1);
   expect(availableProducts[1].ProductId).toBe(productId2);
   expect(availableProducts.length).toBe(2);
 });
 
-test('list provisioning artifacts return list of provisioning artifact details', async () => {
-  const productId = 'prod-abc123';
-  const paId ='pa-abc123';
-
-  client.listProvisioningArtifacts = jest.fn().mockImplementation(async params => {
-    expect(params.ProductId).toBe(productId);
-    return {
-      ProvisioningArtifactDetails: [
-        {
-          Id: paId,
-          Name: 'v1.0',
-          Description: 'description',
-        },
-      ],
-    };
-  });
-
-  const provisioningArtifacts = await testee.listProvisioningArtifacts(productId, { client });
-  expect(provisioningArtifacts[0].Id).toBe(paId);
-  expect(provisioningArtifacts.length).toBe(1);
-});
-
-test('list provisioning artifacts returns query provisioning artifact detail', async () => {
-  const productId = 'prod-abc123';
-  const paId ='pa-abc123';
-  const queryPaId ='pa-query123';
-
-  client.listProvisioningArtifacts = jest.fn().mockImplementation(async params => {
-    expect(params.ProductId).toBe(productId);
-    return {
-      ProvisioningArtifactDetails: [
-        {
-          Id: paId,
-          Name: 'v1.0',
-          Description: 'description1',
-        },
-        {
-          Id: queryPaId,
-          Name: 'v2.0',
-          Description: 'description2',
-        },
-      ],
-    };
-  });
-
-  const provisioningArtifacts = await testee.listProvisioningArtifacts(productId, {
-    client: client,
-    provisioningArtifactId: queryPaId,
-  });
-  expect(provisioningArtifacts[0].Id).toBe(queryPaId);
-  expect(provisioningArtifacts.length).toBe(1);
-});
-
-test('list launch paths should fetch query launch path', async () => {
+test('describe product aggregate should create product data aggregate ', async () => {
   const productId = 'prod-abc123';
   const launchPathId = 'lp-abc123';
+  const provisioningArtifactId = 'pa-abc123';
+  const provisioningArtifactParameters = [
+    {
+      ParameterKey: 'Key',
+      ParameterType: 'String',
+      Description: 'Parameter Description',
+    },
+  ];
 
-  client.listLaunchPaths = jest.fn().mockImplementation(async params => {
-    expect(params.ProductId).toBe(productId);
+  client.describeProduct = jest.fn().mockImplementation( async params => {
+    expect(params.Id).toBe(productId);
     return {
-      LaunchPathSummaries: [{
+      ProductViewSummary: {
+        ProductId: productId,
+      },
+      ProvisioningArtifacts: [{
+        Id: provisioningArtifactId,
+        Name: 'v1.0',
+        Description: 'description',
+      }],
+      LaunchPaths: [{
         Id: launchPathId,
       }],
     };
   });
 
-  const launchPaths = await testee.listLaunchPaths(productId, {
-    client: client,
+  client.describeProvisioningParameters = jest.fn().mockImplementation( () => {
+    return {
+      ProvisioningArtifactParameters: provisioningArtifactParameters,
+    };
+  });
+
+  const describedProduct = await testee.describeProductAggregate( {
+    productId: productId,
     launchPathId: launchPathId,
-  });
-  expect(launchPaths[0].Id).toBe(launchPathId);
-  expect(launchPaths.length).toBe(1);
-});
-
-test('list launch paths should handle pagination', async () => {
-  const productId = 'prod-abc123';
-  const launchPathId1 = 'lp-abc123';
-  const launchPathId2 = 'lp-abc456';
-
-  client.listLaunchPaths = jest.fn().mockImplementation(async params => {
-    expect(params.ProductId).toBe(productId);
-    if (!params.PageToken) {
-      return {
-        NextPageToken: 'NextPageToken',
-        LaunchPathSummaries: [{
-          Id: launchPathId1,
-        }],
-      };
-    } else if (params.PageToken === 'NextPageToken') {
-      return {
-        LaunchPathSummaries: [{
-          Id: launchPathId2,
-        }],
-      };
-    } else {
-      throw new Error('Invalid pagination token');
-    }
+    provisioningArtifactId: provisioningArtifactId,
+    client: client,
   });
 
-  const launchPaths = await testee.listLaunchPaths(productId, { client: client });
-  expect(launchPaths[0].Id).toBe(launchPathId1);
-  expect(launchPaths[1].Id).toBe(launchPathId2);
-  expect(launchPaths.length).toBe(2);
+  expect(describedProduct.product.ProductId).toBe(productId);
 });
 
 test('should describe provisioning parameters', async () => {
@@ -187,14 +124,177 @@ test('should describe provisioning parameters', async () => {
     };
   });
 
-  const provisioningParameters = await testee.describeProvisioningParameters(
-    productId,
-    provisioningArtifactId,
-    launchPathId,
-    { client: client },
-  );
+  const provisioningParameters = await testee.describeProvisioningParameters( {
+    productId: productId,
+    provisioningArtifactId: provisioningArtifactId,
+    launchPathId: launchPathId,
+    client: client,
+  });
 
   expect(provisioningParameters.ProvisioningArtifactParameters).toBe(provisioningArtifactParameters);
+});
+
+test('describe product aggregate handle default artifact paths', async () => {
+  const productId = 'prod-abc123';
+  const provisioningArtifactId = 'pa-abc123';
+
+  client.describeProduct = jest.fn().mockImplementation( async params => {
+    expect(params.Id).toBe(productId);
+    return {
+      ProductViewSummary: {
+        ProductId: productId,
+      },
+      ProvisioningArtifacts: [
+        {
+          Id: 'pa-abc456',
+          Name: 'v2.0',
+          Description: 'description',
+        },
+        {
+          Id: provisioningArtifactId,
+          Name: 'v1.0',
+          Description: 'description',
+          Guidance: 'DEFAULT',
+        },
+      ],
+      LaunchPaths: [{
+        Id: 'lp-abc123',
+      }],
+    };
+  });
+  client.describeProvisioningParameters = jest.fn().mockImplementation( () => {
+    return {
+      ProvisioningArtifactParameters: [],
+    };
+  });
+
+  const describedProduct = await testee.describeProductAggregate( {
+    productId: productId,
+    client: client,
+  });
+
+  expect(describedProduct.product.ProductId).toBe(productId);
+  expect(describedProduct.provisioningArtifact.Id).toBe(provisioningArtifactId);
+});
+
+test('describe product aggregate handle most recently created artifact paths', async () => {
+  const productId = 'prod-abc123';
+  const provisioningArtifactId = 'pa-abc123';
+
+  client.describeProduct = jest.fn().mockImplementation( async params => {
+    expect(params.Id).toBe(productId);
+    return {
+      ProductViewSummary: {
+        ProductId: productId,
+      },
+      ProvisioningArtifacts: [
+        {
+          Id: 'pa-abc456',
+          Name: 'v2.0',
+          Description: 'description',
+          CreatedTime: '2022-02-15T16:56:31-05:00',
+        },
+        {
+          Id: provisioningArtifactId,
+          Name: 'v1.0',
+          Description: 'description',
+          CreatedTime: '2022-02-16T16:56:31-05:00',
+        },
+      ],
+      LaunchPaths: [{
+        Id: 'lp-abc123',
+      }],
+    };
+  });
+  client.describeProvisioningParameters = jest.fn().mockImplementation( () => {
+    return {
+      ProvisioningArtifactParameters: [],
+    };
+  });
+
+  const describedProduct = await testee.describeProductAggregate( {
+    productId: productId,
+    client: client,
+  });
+
+  expect(describedProduct.product.ProductId).toBe(productId);
+  expect(describedProduct.provisioningArtifact.Id).toBe(provisioningArtifactId);
+});
+
+test('describe product aggregate should resolve launch path', async () => {
+  const productId = 'prod-abc123';
+  const provisioningArtifactId = 'pa-abc123';
+  const launchPathId = 'lp-abc123';
+
+  client.describeProduct = jest.fn().mockImplementation( async params => {
+    expect(params.Id).toBe(productId);
+    return {
+      ProductViewSummary: {
+        ProductId: productId,
+      },
+      ProvisioningArtifacts: [
+        {
+          Id: 'pa-abc456',
+          Name: 'v2.0',
+          Description: 'description',
+        },
+        {
+          Id: provisioningArtifactId,
+          Name: 'v1.0',
+          Description: 'description',
+          Guidance: 'DEFAULT',
+        },
+      ],
+      LaunchPaths: [{
+        Id: launchPathId,
+      }],
+    };
+  });
+  client.describeProvisioningParameters = jest.fn().mockImplementation( () => {
+    return {
+      ProvisioningArtifactParameters: [],
+    };
+  });
+
+  const describedProduct = await testee.describeProductAggregate( {
+    productId: productId,
+    client: client,
+  });
+
+  expect(describedProduct.product.ProductId).toBe(productId);
+  expect(describedProduct.provisioningArtifact.Id).toBe(provisioningArtifactId);
+  expect(describedProduct.launchPath.Id).toBe(launchPathId);
+});
+
+test('should fail to resolvable multiple launch paths ', async () => {
+  const productId = 'prod-abc123';
+
+  client.describeProduct = jest.fn().mockImplementation( async params => {
+    expect(params.Id).toBe(productId);
+    return {
+      ProductViewSummary: {
+        ProductId: productId,
+      },
+      ProvisioningArtifacts: [
+        {
+          Id: 'pa-abc123',
+          Name: 'v1.0',
+          Description: 'description',
+        },
+      ],
+      LaunchPaths: [{
+        Id: 'lp-abc123',
+      },
+      {
+        Id: 'lp-abc456',
+      }],
+    };
+  });
+
+  await expect(testee.describeProductAggregate( {
+    productId: productId,
+    client: client,
+  })).rejects.toThrow(/Unable to resolve between multiple launch paths./);
 });
 
 afterEach(() => {
